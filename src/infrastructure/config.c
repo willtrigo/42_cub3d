7c5b93d983055d2c3f686609beead6a6b074ef71
willtrigo <willtrigo@gmail.com>
2025-02-17 19:10:23 -0300

    Feat: Validation of number argument.  Changes to be committed: 	modified:   src/infrastructure/config.c

--- src/infrastructure/config.c
+++ src/infrastructure/config.c
@@ -6,19 +6,19 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 18:31:35 by dande-je          #+#    #+#             */
-/*   Updated: 2025/02/17 18:40:18 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/02/17 19:09:42 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "infrastructure/config.h"
+#include "ft_stdio.h"
 #include "infrastructure/parse_file.h"
 
 static int	is_invalid_args(int argc, char **argv);
 
 int	config_init(int argc, char **argv, t_config_file *config)
 {
-	
 	if (is_invalid_args(argc, argv) == EXIT_FAILURE)
 		return (EXIT_FAILURE);
 	if (parse_file(*argv, config) == EXIT_FAILURE)
@@ -33,7 +33,13 @@ void	config_clean(t_config_file *config)
 
 static int	is_invalid_args(int argc, char **argv)
 {
-	//TODO: validade argc and filename on arg[0] 
+	if (argc != 1)
+	{
+		ft_puterrl("Error: invalid number of arguments");
+		return (EXIT_FAILURE);
+	}
+	(void)argv;
+	//TODO: validade filename on arg[0] 
 	return (EXIT_SUCCESS);
 }
 
