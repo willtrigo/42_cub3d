03d68557053c632542b9f273bb51cc27926d3e62
willtrigo <willtrigo@gmail.com>
2025-02-19 18:08:37 -0300

    Fix: Change the return of parse_textures to true/false  Changes to be committed: 	modified:   src/infrastructure/config/parse/parse_file.c 	modified:   src/infrastructure/config/parse/parse_internal.h

--- src/infrastructure/config/parse/parse_file.c
+++ src/infrastructure/config/parse/parse_file.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 16:34:24 by dande-je          #+#    #+#             */
-/*   Updated: 2025/02/19 17:41:19 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/02/19 18:08:01 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ bool	parse_file(char *filename, t_config_file *config)
 	fd = open(filename, O_RDONLY, CHMOD_PERMISSION);
 	if (fd == FD_FAIL)
 		return (false);
-	if (parse_textures(fd, config) == EXIT_FAILURE)
+	if (!parse_textures(fd, config))
 		return (clean_parse(config, fd, false));
 	if (parse_colors(fd, config) == EXIT_FAILURE)
 		return (clean_parse(config, fd, false));
@@ -34,7 +34,7 @@ bool	parse_file(char *filename, t_config_file *config)
 	return (close_fd_ret(fd, true));
 }
 
-int	parse_textures(int file_fd, t_config_file *config)
+bool	parse_textures(int file_fd, t_config_file *config)
 {
 	char	*line;
 	(void) config; // TODO parse textures
@@ -43,7 +43,7 @@ int	parse_textures(int file_fd, t_config_file *config)
 	{
 		line = get_next_line(file_fd);
 		if (line == 0)
-			return (EXIT_FAILURE);
+			return (false);
 		if (ft_is_blank(line))
 		{
 			// TODO: ? support multiple blank lines
@@ -52,7 +52,7 @@ int	parse_textures(int file_fd, t_config_file *config)
 		}
 		free(line);
 	}
-	return (EXIT_SUCCESS);
+	return (true);
 }
 
 int	clean_parse(t_config_file *config, int file_fd, int ret)
