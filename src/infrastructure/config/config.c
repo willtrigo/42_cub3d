6ff929f3986c75750d1d280438f0eb0efe87f2b2
dande-je <dande-je@student.42sp.org.br>
2025-02-24 16:09:05 -0300

    Fix: remove unused logic.  Changes to be committed: 	modified:   Makefile 	modified:   src/infrastructure/config/config.c 	deleted:    src/infrastructure/config/validation/invalid_config.c 	deleted:    src/infrastructure/config/validation/invalid_config.h

--- src/infrastructure/config/config.c
+++ src/infrastructure/config/config.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 18:31:35 by dande-je          #+#    #+#             */
-/*   Updated: 2025/02/22 12:32:32 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/02/24 16:08:07 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 #include "ft_memlib.h"
 #include "infrastructure/config/args/invalid_args.h"
 #include "infrastructure/config/parse/parse_file.h"
-#include "infrastructure/config/validation/invalid_config.h"
 #include <stdbool.h>
 
 bool	config_init(int argc, char **argv, t_config_file *config)
@@ -23,13 +22,6 @@ bool	config_init(int argc, char **argv, t_config_file *config)
 	if (!is_invalid_args(argc, *argv))
 		return (false);
 	if (!parse_file(*argv, config))
-		return (false);
-	return (true);
-}
-
-bool	config_validation(t_config_file *config)
-{
-	if (!is_invalid_config(config))
 		return (false);
 	return (true);
 }
