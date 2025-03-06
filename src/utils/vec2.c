5de91bed614a0abfab7bd510e0deef9fc9d7e5d5
RedJocker <mauricio.guaruja@gmail.com>
2025-03-06 19:48:37 -0300

    Refactor: normitete vec2

--- src/utils/vec2.c
+++ src/utils/vec2.c
@@ -6,7 +6,7 @@
 /*   By: maurodri <maurodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 15:21:24 by maurodri          #+#    #+#             */
-/*   Updated: 2025/03/01 12:52:46 by maurodri         ###   ########.fr       */
+/*   Updated: 2025/03/06 19:47:55 by maurodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,52 +15,52 @@
 
 t_vec2i	vec2i_scale(t_vec2i vec, int scalar)
 {
-	return (t_vec2i) { vec.x * scalar, vec.y * scalar};
+	return ((t_vec2i){vec.x * scalar, vec.y * scalar});
 }
 
 t_vec2f	vec2f_scalei(t_vec2f vec, int scalar)
 {
-	return (t_vec2f) { vec.x * scalar, vec.y * scalar};
+	return ((t_vec2f){vec.x * scalar, vec.y * scalar});
 }
 
 t_vec2f	vec2f_scale(t_vec2f vec, float scalar)
 {
-	return (t_vec2f) { vec.x * scalar, vec.y * scalar};
+	return ((t_vec2f){vec.x * scalar, vec.y * scalar});
 }
 
 t_vec2i	vec2i_add(t_vec2i vec_a, t_vec2i vec_b)
 {
-	return (t_vec2i) { vec_a.x + vec_b.x, vec_a.y + vec_b.y};
+	return ((t_vec2i){vec_a.x + vec_b.x, vec_a.y + vec_b.y});
 }
 
 t_vec2f	vec2i_tof(t_vec2i vec_a)
 {
-	return (t_vec2f) {vec_a.x, vec_a.y};
+	return ((t_vec2f){vec_a.x, vec_a.y});
 }
 
 t_vec2i	vec2i_addf(t_vec2i vec_a, t_vec2f vec_b)
 {
-	return (t_vec2i) { vec_a.x + (int) vec_b.x, vec_a.y + (int) vec_b.y};
+	return ((t_vec2i){vec_a.x + (int) vec_b.x, vec_a.y + (int) vec_b.y});
 }
 
 t_vec2f	vec2f_add(t_vec2f vec_a, t_vec2f vec_b)
 {
-	return (t_vec2f) { vec_a.x + vec_b.x, vec_a.y + vec_b.y};
+	return ((t_vec2f){vec_a.x + vec_b.x, vec_a.y + vec_b.y});
 }
 
 t_vec2f	vec2f_addi(t_vec2f vec_a, t_vec2i vec_b)
 {
-	return (t_vec2f) { vec_a.x + vec_b.x, vec_a.y + vec_b.y};
+	return ((t_vec2f){vec_a.x + vec_b.x, vec_a.y + vec_b.y});
 }
 
 t_vec2i	vec2i_offset(t_vec2i vec_a, int scalar)
 {
-	return (t_vec2i) { vec_a.x + scalar, vec_a.y + scalar};
+	return ((t_vec2i){vec_a.x + scalar, vec_a.y + scalar});
 }
 
 t_vec2f	vec2f_offset(t_vec2f vec_a, float scalar)
 {
-	return (t_vec2f) { vec_a.x + scalar, vec_a.y + scalar};
+	return ((t_vec2f){vec_a.x + scalar, vec_a.y + scalar});
 }
 
 float	vec2f_magnitude2(t_vec2f vec_a)
