489fdba8aa659fec8f32bb17b59ef2ba8ec4cbbf
RedJocker <mauricio.guaruja@gmail.com>
2025-04-09 22:56:05 -0300

    Test: prepare for testing valid map formats

--- test/test_suite.c
+++ test/test_suite.c
@@ -6,13 +6,21 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 20:24:50 by dande-je          #+#    #+#             */
-/*   Updated: 2025/02/20 12:40:55 by maurodri         ###   ########.fr       */
+/*   Updated: 2025/04/09 22:53:28 by maurodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "ft_stdio.h"
 #include "test_suite.h"
+
+void test_map() {
+	//TODO: add tests for valid maps
+	// test/maps/diamond.cub
+	// test/maps/diamond_leftpad.cub
+	// test/maps/diamond_trailing_spaces.cub
+	return ;
+}
 
 int	main(void)
 {
