9115de0e3da7b601aa87cf16285746f9b495b0d7
redjocker <mauricio.guaruja@gmail.com>
2025-04-16 06:49:38 -0300

    Wip: drawing bullet

--- src/graphic/render.c
+++ src/graphic/render.c
@@ -6,12 +6,13 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 16:41:04 by dande-je          #+#    #+#             */
-/*   Updated: 2025/04/14 16:21:09 by maurodri         ###   ########.fr       */
+/*   Updated: 2025/04/16 06:49:30 by maurodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "graphic/render.h"
 #include "core/game.h"
+#include "graphic/draw_primitive.h"
 #include "graphic/grid.h"
 #include "utils/vec2.h"
 #include "draw.h"
@@ -35,7 +36,7 @@ void	draw_entities(t_game *game)
 	if (!game->bullet.is_alive)
 		return ;
 	const t_vec2f diff = vec2f_sub(game->bullet.loc.pos, game->player.loc.pos);
-	float	angle = atan2(diff.y, diff.x) - game->player.loc.angle;
+	float	angle =  atan2(diff.y, diff.x) - game->player.loc.angle;
 
 	if (angle < -M_PI)
 		angle += 2 * M_PI;
@@ -52,16 +53,20 @@ void	draw_entities(t_game *game)
 				vec2f_sub(wall.pos, game->player.loc.pos)));
 		float bullet_dist = fabs(vec2f_dot_product(vec2f_unit_vector(angle + game->player.loc.angle), \
 			diff));
-		t_vec2f bullet = vec2f_scale(vec2f_unit_vector(angle - M_PI_2), bullet_dist);
-
+		t_vec2f bullet = vec2f_scale(vec2f_unit_vector(angle + M_PI_2), bullet_dist);
+		bullet.x *= -1;
+		/* float bullet_y = vec2f_dot_product(diff, vec2f_unit_vector(game->player.loc.angle)); */
+		/* float bullet_x = vec2f_dot_product(diff, vec2f_unit_vector(game->player.loc.angle + M_PI_2)); */
 		printf("bullet_dist %.2f, bullet_dist_x %.2f, bullet_dist_y %.2f, diff_y %.2f\n", bullet_dist, bullet.x, bullet.y, diff.y);
 		if (bullet_dist < wall_dist)
 		{
 			printf("is visible\n");
+			int px_x = ((cosf(game->player.fov * 0.5f) * bullet.x / bullet.y) * (game->ctx.window.width)) + (0.5 * game->ctx.window.width);
+
+			draw_circle_cs(game->ctx.canvas,
+						   (t_vec2f){px_x, game->ctx.window.height * 0.5f}, (t_brush) {0xFFFFFFFF, 4.0 / bullet.y});
 		}
 	}
-
-
 }
 
 void	render(t_game *game)
