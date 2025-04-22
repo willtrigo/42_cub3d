484a3f9299acc77981fe82d2537caa8c008425d6
redjocker <mauricio.guaruja@gmail.com>
2025-04-21 21:07:59 -0300

    Test: add diamond map tests

--- test/test_map_init.c
+++ test/test_map_init.c
@@ -6,7 +6,7 @@
 /*   By: maurodri <maurodri@student.42sp...>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 20:04:30 by maurodri          #+#    #+#             */
-/*   Updated: 2025/04/21 19:05:39 by maurodri         ###   ########.fr       */
+/*   Updated: 2025/04/21 19:40:59 by maurodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -376,20 +376,194 @@ void test_chart_trivial_trailing_spaces(void)
 	ft_printf("%s: END\n", __FUNCTION__);
 }
 
+void test_chart_diamond(void)
+{
+	ft_printf("\n%s: START\n", __FUNCTION__);
+	{
+		t_config_file	conf;
+		int res_config = config_init(1,
+					(char *[]){ "./test/maps/diamond.cub", NULL},
+					&conf);
+		FT_TEST(res_config == true,
+				"when filename == \"./test/maps/diamond.cub\" "
+				"expected "
+				"res_config == true");
+		t_game game;
+		ft_bzero(&game, sizeof(t_game));
+		map_init(&game, &conf);
+		FT_TEST(game.manager.chart.dimen.x == 7,
+				"when filename == \"./test/maps/diamond.cub\" "
+				"expected "
+				"game.manager.chart.dimen.x == 7");
+		FT_TEST(game.manager.chart.dimen.y == 11,
+				"when filename == \"./test/maps/diamond.cub\" "
+				"expected "
+				"game.manager.chart.dimen.y == 11");
+		FT_TEST(ft_strncmp(game.manager.chart.buffer,
+								"   1   "
+								"  101  "
+								" 10001 "
+								"1000001"
+								"1000001"
+								"1010101"
+								"1001001"
+								"1010101"
+								" 10001 "
+								"  101  "
+								"   1   "
+							,(7 * 11)) == 0,
+				"when filename == \"./test/maps/diamond.cub\" "
+				"expected "
+				"ft_strncmp(game.manager.chart.buffer,"
+								"   1   "
+								"  101  "
+								" 10001 "
+								"1000001"
+								"1000001"
+								"1010101"
+								"1001001"
+								"1010101"
+								" 10001 "
+								"  101  "
+								"   1   "
+							",(7 * 11)) == 0");
+		if (res_config)
+			config_clean(&conf);
+		game_clean(&game);
+	}
+
+	ft_printf("%s: END\n", __FUNCTION__);
+}
+
+void test_chart_diamond_leftpad(void)
+{
+	ft_printf("\n%s: START\n", __FUNCTION__);
+	{
+		t_config_file	conf;
+		int res_config = config_init(1,
+					(char *[]){ "./test/maps/diamond_leftpad.cub", NULL},
+					&conf);
+		FT_TEST(res_config == true,
+				"when filename == \"./test/maps/diamond_leftpad.cub\" "
+				"expected "
+				"res_config == true");
+		t_game game;
+		ft_bzero(&game, sizeof(t_game));
+		map_init(&game, &conf);
+		FT_TEST(game.manager.chart.dimen.x == 7,
+				"when filename == \"./test/maps/diamond_leftpad.cub\" "
+				"expected "
+				"game.manager.chart.dimen.x == 7");
+		FT_TEST(game.manager.chart.dimen.y == 11,
+				"when filename == \"./test/maps/diamond_leftpad.cub\" "
+				"expected "
+				"game.manager.chart.dimen.y == 11");
+		FT_TEST(ft_strncmp(game.manager.chart.buffer,
+								"   1   "
+								"  101  "
+								" 10001 "
+								"1000001"
+								"1000001"
+								"1010101"
+								"1001001"
+								"1010101"
+								" 10001 "
+								"  101  "
+								"   1   "
+							,(7 * 11)) == 0,
+				"when filename == \"./test/maps/diamond_leftpad.cub\" "
+				"expected "
+				"ft_strncmp(game.manager.chart.buffer,"
+								"   1   "
+								"  101  "
+								" 10001 "
+								"1000001"
+								"1000001"
+								"1010101"
+								"1001001"
+								"1010101"
+								" 10001 "
+								"  101  "
+								"   1   "
+							",(7 * 11)) == 0");
+		if (res_config)
+			config_clean(&conf);
+		game_clean(&game);
+	}
+
+	ft_printf("%s: END\n", __FUNCTION__);
+}
+
+void test_chart_diamond_trailing_spaces(void)
+{
+	ft_printf("\n%s: START\n", __FUNCTION__);
+	{
+		t_config_file	conf;
+		int res_config = config_init(1,
+					(char *[]){ "./test/maps/diamond_trailing_spaces.cub", NULL},
+					&conf);
+		FT_TEST(res_config == true,
+				"when filename == \"./test/maps/diamond_trailing_spaces.cub\" "
+				"expected "
+				"res_config == true");
+		t_game game;
+		ft_bzero(&game, sizeof(t_game));
+		map_init(&game, &conf);
+		FT_TEST(game.manager.chart.dimen.x == 7,
+				"when filename == \"./test/maps/diamond_trailing_spaces.cub\" "
+				"expected "
+				"game.manager.chart.dimen.x == 7");
+		FT_TEST(game.manager.chart.dimen.y == 11,
+				"when filename == \"./test/maps/diamond_trailing_spaces.cub\" "
+				"expected "
+				"game.manager.chart.dimen.y == 11");
+		FT_TEST(ft_strncmp(game.manager.chart.buffer,
+								"   1   "
+								"  101  "
+								" 10001 "
+								"1000001"
+								"1000001"
+								"1010101"
+								"1001001"
+								"1010101"
+								" 10001 "
+								"  101  "
+								"   1   "
+							,(7 * 11)) == 0,
+				"when filename == \"./test/maps/diamond_trailing_spaces.cub\" "
+				"expected "
+				"ft_strncmp(game.manager.chart.buffer,"
+								"   1   "
+								"  101  "
+								" 10001 "
+								"1000001"
+								"1000001"
+								"1010101"
+								"1001001"
+								"1010101"
+								" 10001 "
+								"  101  "
+								"   1   "
+							",(7 * 11)) == 0");
+		if (res_config)
+			config_clean(&conf);
+		game_clean(&game);
+	}
+
+	ft_printf("%s: END\n", __FUNCTION__);
+}
+
 void test_map_init(void)
 {
 	ft_printf("\n%s: START", __FUNCTION__);
-
-	//TODO: add tests for valid maps
-	// test/maps/diamond.cub
-	// test/maps/diamond_leftpad.cub
-	// test/maps/diamond_trailing_spaces.cub
-
 	test_invalid_mapline();
 	test_player_direction();
 	test_player_pos();
 	test_chart_trivial();
 	test_chart_trivial_left_pad();
 	test_chart_trivial_trailing_spaces();
+	test_chart_diamond();
+	test_chart_diamond_leftpad();
+	test_chart_diamond_trailing_spaces();
 	ft_printf("%s: END\n", __FUNCTION__);
 }
